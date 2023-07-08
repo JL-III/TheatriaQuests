a228b80c7b59cc0c406494d5719a88b4d4221f25
jl-iii <jesselopez1162@gmail.com>
2023-07-08 10:57:41 -0500

    bug fixes in quests, add more functionality to zip.go

--- util/zip.go
+++ util/zip.go
@@ -3,13 +3,34 @@ package main
 import (
 	"fmt"
 	"github.com/mholt/archiver/v3"
+	"log"
+	"os"
 )
 
 func main() {
-	err := archiver.Archive([]string{"../QuestPackages", "../QuestTemplates"}, "quests.zip" )
+
+	zipName := "quests.zip"
+
+	err := deleteFileIfExists(zipName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = archiver.Archive([]string{"../QuestPackages", "../QuestTemplates"}, zipName)
 	if err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Println("Directories successfully zipped!")
+	}
+}
+
+func deleteFileIfExists(filename string) error {
+	if _, err := os.Stat(filename); err == nil {
+		fmt.Println("Found existing zip... removing.")
+		return os.Remove(filename)
+	} else if os.IsNotExist(err) {
+		return nil
+	} else {
+		return err
 	}
 }
