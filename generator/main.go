d19b2dd8adb8d962ebae51b247e973ccaf01bdd9
jl-iii <jesselopez1162@gmail.com>
2023-07-11 22:09:47 -0500

    make main.go the entry point for different functionality

--- generator/main.go
+++ generator/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"quests/menu"
+	"quests/utils"
 )
-
 
 func main() {
 
@@ -12,15 +12,27 @@ func main() {
 	blue := "\033[34m"
 	reset := "\033[0m"
 
-	fmt.Print(blue + "Template type: " + yellow)
-	var template_type string
-	fmt.Scanln(&template_type)
+	fmt.Print(yellow + "Choose a function - [menu,zip,schedule]: " + reset)
+	var choice string
+	fmt.Scanln(&choice)
 
-	template_path := "../QuestPackages/daily/" + template_type
-	fmt.Print(blue + "Level: " + yellow)
-	fmt.Print(reset)
-	var level string
-	fmt.Scanln(&level)
-	menu.CreateMenu(template_path, level, template_type)
+	if choice == "menu" {
+		fmt.Print(blue + "Template type: " + yellow)
+		var template_type string
+		fmt.Scanln(&template_type)
+
+		template_path := "../QuestPackages/daily/" + template_type
+		fmt.Print(blue + "Level: " + yellow)
+		fmt.Print(reset)
+		var level string
+		fmt.Scanln(&level)
+		menu.CreateMenu(template_path, level, template_type)
+	} else if choice == "zip" {
+		utils.Zip()
+	} else if choice == "schedule" {
+		fmt.Print("You chose the schedule option!")
+	} else {
+		fmt.Print("You did not choose an available option.")
+	}
 
 }
