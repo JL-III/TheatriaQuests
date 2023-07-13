db8b210cca9d97d48aad733c86336ef845aad95e
jl-iii <jesselopez1162@gmail.com>
2023-07-12 21:26:44 -0500

    finish autogenerated schedule resets

--- generator/schedules/schedules.go
+++ generator/schedules/schedules.go
@@ -1,8 +1,8 @@
 package schedules
 
 import (
-	"fmt"
 	"log"
+	"strings"
 	"quests/utils"
 )
 
@@ -13,28 +13,58 @@ func CreateScheduleEntries(base_path string) {
 		log.Fatal(err)
 	}
 
-	fmt.Println(base_path)
+	utils.DeleteFileIfExists(base_path+"schedules/package.yml")
+
+	var schedulesBase strings.Builder
+
+	schedulesBase.WriteString("schedules:\n")
+	schedulesBase.WriteString("  resetDailyQuests:\n")
+	schedulesBase.WriteString("    type: realtime-daily\n")
+	schedulesBase.WriteString("    time: '03:05'\n")
+	schedulesBase.WriteString("    events: announceResetDaily,resetDaily\n\n")
+	schedulesBase.WriteString("events:\n")
+	schedulesBase.WriteString("  announceResetDaily: notifyall &eDaily Quests reset!\n")
+
+
+	utils.Write(base_path+"schedules/package.yml", schedulesBase.String())
+
+
+	var resetDaily strings.Builder
+
+	for i, filename := range filenames {
+		if i == len(filenames)-1 {
+			resetDaily.WriteString(filename+"Reset")
+		} else {
+			resetDaily.WriteString(filename+"Reset"+",")
+		}
+	}
+	utils.Write(base_path+"schedules/package.yml", "  resetDaily: " + "folder " + resetDaily.String() + " period:5\n")
 
 	for _, filename := range filenames {
+		
 		dirs, err := utils.GetLevelDirectories(base_path + "/" + filename)
 		if err != nil {
 			log.Printf("Error getting directories from path %s: %v\n", base_path, err)
 			continue
 		}
-		fmt.Print(filename + "\n")
-		utils.Write(base_path+"schedules/package.yml", "\n"+filename+"Reset: ")
+
+		utils.Write(base_path+"schedules/package.yml", "\n  "+filename+"Reset: ")
 		for _, dir := range dirs {
 			lastDirs, err := utils.GetLastDirectories(base_path + "/" + filename + "/" + dir)
 			if err != nil {
 				log.Printf("Error getting directories from path %s: %v\n", base_path, err)
 				continue
 			}
-			for _, lastDir := range lastDirs {
+			for i, lastDir := range lastDirs {
 				if err != nil {
 					log.Printf("Error getting directories from path %s: %v\n", base_path, err)
 					continue
 				}
-				utils.Write(base_path+"schedules/package.yml", "daily-"+filename+"-"+dir+"-"+lastDir+".reset,")
+				if i == len(lastDirs)-1 {
+					utils.Write(base_path+"schedules/package.yml", "daily-"+filename+"-"+dir+"-"+lastDir+".reset")
+				} else {
+					utils.Write(base_path+"schedules/package.yml", "daily-"+filename+"-"+dir+"-"+lastDir+".reset,")
+				}
 			}
 		}
 	}
